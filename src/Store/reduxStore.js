f8ede8cbf8645e7ce4e9a2928a288116d134cc54
Salvatore Viscuso <salvo.visk@gmail.com>
2021-03-25 17:42:39 +0100

    .

--- src/Store/reduxStore.js
+++ src/Store/reduxStore.js
@@ -3,4 +3,8 @@ import themeReducer from "./themeReducer";
 
 const defaultStore = { theme: "light" };
 
-export const store = createStore(themeReducer, defaultStore);
+export const store = createStore(
+  themeReducer,
+  defaultStore,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
